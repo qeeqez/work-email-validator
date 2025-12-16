fe9ff1ee9fc17568ef590e4192c05042ec09e111
Maksim Sasnouski <qeeqez@gmail.com>
2025-12-17 02:16:16 +0400

    refactor: move benches

--- validator_test.go
+++ validator_test.go
@@ -116,21 +116,3 @@ func TestNormalizeDomain(t *testing.T) {
 		})
 	}
 }
-
-func BenchmarkIsDisposableDomain(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		IsDisposableDomain("temp-mail.com")
-	}
-}
-
-func BenchmarkIsFreeDomain(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		IsFreeDomain("gmail.com")
-	}
-}
-
-func BenchmarkIsBusinessDomain(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		IsBusinessDomain("example.com")
-	}
-}
